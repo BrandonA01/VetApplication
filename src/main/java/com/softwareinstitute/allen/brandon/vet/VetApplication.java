6289b50547a62a52bbd1cb4c3496d96204b2b8a7
Brandon Allen <81233247+BrandonA01@users.noreply.github.com>
2022-08-11 11:45:18 +0100

    New Commit

--- src/main/java/com/softwareinstitute/allen/brandon/vet/VetApplication.java
+++ src/main/java/com/softwareinstitute/allen/brandon/vet/VetApplication.java
@@ -2,7 +2,6 @@ package com.softwareinstitute.allen.brandon.vet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
