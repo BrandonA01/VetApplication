0474d5e13ced60db772ce0f9ffc4cf89ee91728f
Brandon Allen <81233247+BrandonA01@users.noreply.github.com>
2022-08-13 09:40:04 +0100

    Removed test

--- src/test/java/com/softwareinstitute/allen/brandon/vet/tests/VetApplicationTests.java
+++ src/test/java/com/softwareinstitute/allen/brandon/vet/tests/VetApplicationTests.java
@@ -1,25 +1,8 @@
 package com.softwareinstitute.allen.brandon.vet.tests;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.test.web.server.LocalServerPort;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest()
 class VetApplicationTests {
 
-	@LocalServerPort
-	private int port;
-
-	@Autowired
-	private TestRestTemplate restTemplate;
-
-	@Test
-	void checkHTTPResponse() {
-		assertThat(restTemplate.getForObject("http://localhost:" + port + "/test",
-				String.class)).contains("New Test");
-	}
 }
