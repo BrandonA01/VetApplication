63e9e0366204ba04fe28ce0d0e58fbe7935aa8ec
Brandon Allen <81233247+BrandonA01@users.noreply.github.com>
2022-08-13 09:33:52 +0100

    Search method works in vets

--- src/main/java/com/softwareinstitute/allen/brandon/vet/VetApplication.java
+++ src/main/java/com/softwareinstitute/allen/brandon/vet/VetApplication.java
@@ -6,55 +6,50 @@ import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.logging.Logger;
-
 @SpringBootApplication
 @RestController
 public class VetApplication {
 
-	static AnimalRepository animalList = new AnimalRepository();
-	static String zooURL = "http://localhost:1235";
+
 
 	public static void main(String[] args) {
-		animalList.add();
 		SpringApplication.run(VetApplication.class, args);
-		getAllAnimals();
+	}
+
+	@PutMapping("/addNewAnimal")
+	@ResponseBody
+	public String add(@RequestParam String name, @RequestParam String colour, @RequestParam int age, @RequestParam boolean alive) {
+		connectionToZooAnimals("/add", HttpMethod.PUT, "/"+name+"/"+colour+"/"+age+"/"+alive);
+		return connectionToZooAnimals("/search", HttpMethod.GET, "/"+name);
 	}
 
 	@GetMapping("/customRoute")
-	public static @ResponseBody String getAllAnimals() {
-		zooURL += "/allAnimals";
+	public String getAllAnimals(){
+		return connectionToZooAnimals("/allAnimals", HttpMethod.GET);
+	}
+
+	public String connectionToZooAnimals(String str, HttpMethod var, String body) {
+		String zooURL = "http://localhost:1235" + str + body;
 		RestTemplate restTemplate = new RestTemplate();
 		ResponseEntity<String> response = null;
 		try{
-			response=restTemplate.exchange(zooURL, HttpMethod.GET, getHeaders(), String.class);
+			response=restTemplate.exchange(zooURL, var, getHeaders(), String.class);
+			return response.getBody().toString();
+
 		}
 		catch(Exception e){
-			System.out.println(e);
+			return e.getMessage();
 		}
-		System.out.println(response.getBody());
-		return response.getBody().toString();
 	}
+
+	public @ResponseBody String connectionToZooAnimals(String str, HttpMethod var) {
+		return connectionToZooAnimals(str, var, "");
+	}
+
 
 	private static HttpEntity<?> getHeaders() {
 		HttpHeaders headers = new HttpHeaders();
 		headers.set("Accept", MediaType.APPLICATION_JSON_VALUE);
 		return new HttpEntity<>(headers);
-	}
-
-	@GetMapping("/test")
-	public @ResponseBody String testHttpResponse() {
-		return "New Test";
-	}
-
-	@GetMapping("/search")
-	public @ResponseBody String getAnimalName(@RequestParam String name) {
-		return animalList.toStringJSON(animalList.searchAnimalByName(name));
-	}
-
-	@PutMapping("/add")
-	@ResponseBody
-	public void add(@RequestParam String name, @RequestParam String colour, @RequestParam int age, @RequestParam boolean alive) {
-		animalList.addAnimal(new Cat(alive, age, name, colour));
 	}
 }
